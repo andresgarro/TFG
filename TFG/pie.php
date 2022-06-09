fdb62ba6006fbc26d9087d8bcf1e85a84a5a3f3a
Andres Garro <andres.garro.soto.97@gmail.com>
2022-06-09 20:09:09 +0200

    pie de mi página

---
+++ TFG/pie.php
@@ -0,0 +1,23 @@
+   <!doctype html>
+   <link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+   <footer>
+      <div id="grid-footer">
+         <div id="contenedor2">
+            <h3>Redes sociales</h3><br>
+            <i class="fab fa-instagram"></i> Instagram<br>
+            <i class="fab fa-twitter"></i> Twitter<br>
+            <i class="fab fa-whatsapp"></i> WhatsApp<br>
+            <i class="fas fa-envelope"></i> Mail<br>
+         </div>
+         <div id="contenedor3">
+            <h3>Sitios</h3><br>
+            <i class="fas fa-map-marked-alt"></i> C/ Herzog Lakes Suite 324<br>
+            <i class="fas fa-thumbtack"></i> CP: 18899<br>
+            <i class="fas fa-phone"></i> Tlf: 667315599<br>
+            <i class="fas fa-user"></i> Andrés Garro Soto
+         </div>
+      </div>
+   </footer>
+   </div>
+  </body>
+</html>
